6706942e99a35130b91ddd93a1928cdffb8253c9
BrownTen <BrownTen.Boy@Gmail.com>
2019-05-21 21:37:08 +0800

    完成教师主界面中siderbar完善

--- src/main/java/com/wyt/labinformationmanagementsystem/controller/TeacherController.java
+++ src/main/java/com/wyt/labinformationmanagementsystem/controller/TeacherController.java
@@ -5,6 +5,7 @@ import com.wyt.labinformationmanagementsystem.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,6 +18,11 @@ public class TeacherController {
     @Autowired
     TeacherService teacherService;
 
+    @GetMapping
+    public String toTeacherSelfInfo(){
+        return "teacher/infos/self";
+    }
+
     @PutMapping
     public String updateTeacher(Teacher teacher,
                                 Model model,
@@ -27,4 +33,23 @@ public class TeacherController {
         return "self";
     }
 
+    @GetMapping("/courses")
+    public String findAllCoursesByTeacherId(){
+        return "teacher/infos/course";
+    }
+
+    @GetMapping("/labs")
+    public String findAllLabs(){
+        return "teacher/infos/orderlab";
+    }
+
+    @GetMapping("/orderedLabs")
+    public String findAllOrderedLabs(){
+        return "teacher/infos/orderRecord";
+    }
+
+    @GetMapping("/reports")
+    public String findAllReports(){
+        return "teacher/infos/report";
+    }
 }
